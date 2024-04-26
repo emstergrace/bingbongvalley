55c5d91f622217b06fb0f40e69d0bd708c180d7f
Brazen A <brazen.abelgas@gmail.com>
2024-04-26 16:06:58 -0700

    At some point I'll get off of main. Today is not that day. Pushing audio manager and getting rid of unity audio stuff

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,122 +1,68 @@
 using UnityEngine;
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Inst { get; private set; }
 
-	#region sounds here
-	// ADD SOUNDS BELOW
 	[Header("Sounds")]
-	[SerializeField] private AudioSource walkNoise = null; public void SetWalkSource(AudioSource aSO) { walkNoise = aSO; }
-	[SerializeField] private List<AudioClip> footNoises = new List<AudioClip>();
-
-	[SerializeField] private List<Sound> sounds = new List<Sound>(); 
-	public Dictionary<string, AudioSource> SoundsDictionary { get; private set; } = new Dictionary<string, AudioSource>();
-
-	public float walkNoiseBuffer = 1f;
-	private float walkNoiseDelay = 1f;
-
-	#endregion
-
-	public void Walk() {
-		if (walkNoiseDelay <= walkNoiseBuffer) {
-			walkNoiseDelay += Time.deltaTime;
-		}
-		if (walkNoise != null && !walkNoise.isPlaying && walkNoiseDelay > walkNoiseBuffer) {
-			footIndex = UnityEngine.Random.Range(0, footNoises.Count);
-			walkNoise.clip = footNoises[footIndex];
-			walkNoise.Play();
-			walkNoiseDelay = 0f;
-		}
-	}
-	public void Sprint(bool val) { if (val && walkNoise.pitch != 2f) walkNoise.pitch = 2f; else if (!val && walkNoise.pitch != 1f) walkNoise.pitch = 1f; }
-	private int footIndex = 0;
-
-	[SerializeField] private List<AudioSource> sceneAudio = new List<AudioSource>();
+	public string soundVCAPath = "vca:/VCA Name"; 
 	private static float soundVolume = 0.5f; public static float Sound { get { return soundVolume; } }
 
 	public Action<float> NewSoundVolume;
 
+	[Header("Ambience")]
+	public string ambienceVCAPath = "vca:/VCA Name";
+	private static float ambienceVolume = 0.5f; public static float Ambience { get { return ambienceVolume; } }
+
+	public Action<float> NewAmbienceVolume;
+
 	[Header("Music")]
-	[SerializeField] private AudioSource menuMusic = null;
-	[SerializeField] private AudioSource mainTheme = null; public void SetMainTheme(AudioSource aSo) { mainTheme = aSo; aSo.Play(); }
+	public string musicVCAPath = "vca:/VCA Name";
 	private static float musicVolume = 0.5f; public static float Music { get { return musicVolume; } }
 
 	private void Awake() {
 		Inst = this;
-		for (int i = 0; i < sounds.Count; i++) {
-			SoundsDictionary.Add(sounds[i].name, sounds[i].audio);
-		}
-	}
+	} // End of Awake().
 
 	private void Start() {
-
 		SetSoundLevel(soundVolume);
 		SetMusicLevel(musicVolume);
-	}
-
-	public void SubscribeSFXAudio(AudioSource source) {
-		sceneAudio.Add(source);
-		source.volume = soundVolume;
-	}
-	public void UnsubscribeSFXAudio(AudioSource source) {
-		sceneAudio.Remove(source);
-	}
+	} // End of Start().
 
 	public void SetSoundLevel(float level) {
-		soundVolume = level;
-		foreach(AudioSource source in sceneAudio) {
-			source.volume = soundVolume;
-		}
-		foreach (KeyValuePair<string, AudioSource> keys in SoundsDictionary) {
-			if (keys.Value != null)
-				keys.Value.volume = level;
-		}
+		// FMOD stuff
+		VCA soundVCA = RuntimeManager.GetVCA(soundVCAPath);
+		soundVCA.setVolume(level);
 
+		soundVolume = level;
 		NewSoundVolume?.Invoke(level);
-	}
+	} // End of SetSoundLevel().
+
+	public void SetAmbienceLevel(float level) {
+		VCA ambVCA = RuntimeManager.GetVCA(ambienceVCAPath);
+		ambVCA.setVolume(level);
+
+		ambienceVolume = level;
+		NewAmbienceVolume?.Invoke(level);
+	} // End of SetAmbienceLevel().
 
 	public void SetMusicLevel(float level) {
-		musicVolume = level;
-		if (menuMusic != null) menuMusic.volume = musicVolume;
-		if (mainTheme != null) mainTheme.volume = musicVolume;
-	}
+		VCA musicVCA = RuntimeManager.GetVCA(musicVCAPath);
+		musicVCA.setVolume(level);
+	} // End of SetMusicLevel().
 
+	// These could totally be redundant, no clue how fmod works
 	public void PlayMainTheme() {
-		mainTheme.Play();
+
 	}
 
 	public void PlaySound(string name) {
-		if (SoundsDictionary.ContainsKey(name)) {
-			if (SoundsDictionary[name] != null)
-				SoundsDictionary[name].Play();
-			else {
-				Debug.LogError("Sounds dictionary contains listing for [" + name + "] but no sound associated");
-			}
-		}
-		else
-			Debug.LogError("Sounds dictionary does not contain sound for " + name);
 	}
 
 	public void StopSound(string name) {
-		if (SoundsDictionary.ContainsKey(name)) {
-			if (SoundsDictionary[name] != null)
-				SoundsDictionary[name].Stop();
-			else {
-				Debug.LogError("Sounds dictionary contains listing for [" + name + "] but no sound associated");
-			}
-		}
-		else
-			Debug.LogError("Sounds dictionary does not contain sound for " + name);
 	}
 
-}
-[System.Serializable]
-public class Sound
-{
-	public string name;
-	public AudioSource audio;
 }
