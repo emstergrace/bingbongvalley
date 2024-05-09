f3390cd1d32e63d85195d15fdd36b5c14824a9ed
Brazen A <brazen.abelgas@gmail.com>
2024-05-09 02:42:37 -0700

    Minor change to MoveToSceneInteractable to allow specific direction facing (for future purposes)

--- Assets/Scripts/Utilities/MoveToSceneInteractable.cs
+++ Assets/Scripts/Utilities/MoveToSceneInteractable.cs
@@ -6,8 +6,17 @@ public class MoveToSceneInteractable : MonoBehaviour, IInteractable
 {
 	public string sceneName;
 	public Vector3 teleportPosition;
+	public FaceDirection directionAfterTeleport;
 
 	public void Interact() {
 		GameManager.LoadScene(sceneName, teleportPosition);
 	}
+}
+
+public enum FaceDirection
+{
+	Up,
+	Down,
+	Left,
+	Right
 }
